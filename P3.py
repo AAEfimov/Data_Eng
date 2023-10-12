ea6bf0087e899ac3b09f585d9290e6a90999f3b5
Aleksey Efimov <efimov.alexey.a@gmail.com>
2023-10-12 21:58:49 +0500

    Update P3.py

--- P3.py
+++ P3.py
@@ -11,6 +11,6 @@ with open(filename) as f:
 with open(filename+"out", "w") as f:
     for l in lines:
         tl = [w for w in l.strip().split(det)]
-        lq = [int(w) if w.isdigit() else int(tl[i-1]) + int(tl[i+1]) for i,w in enumerate(tl)]
+        lq = [int(w) if w.isdigit() else (int(tl[i-1]) + int(tl[i+1])) / 2 for i,w in enumerate(tl)]
         [f.write(f"{i},") if i ** 0.5 >= less_v else "" for i in lq]
         f.write("\n")

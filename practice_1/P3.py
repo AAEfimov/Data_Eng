08c431bafaf81e6814a6165168947bef405bf714
Your Name <a.efimov@infinet.ru>
2023-10-13 08:50:04 +0500

    P3_test1

--- practice_1/P3.py
+++ practice_1/P3.py
@@ -1,6 +1,6 @@
 
 
-filename = "text_3_var_40"
+filename = "tests/text_3_var_40"
 det = ","
 
 less_v = 50 + 40
@@ -10,7 +10,9 @@ with open(filename) as f:
 
 with open(filename+"out", "w") as f:
     for l in lines:
+        # List comprehension
         tl = [w for w in l.strip().split(det)]
-        lq = [int(w) if w.isdigit() else (int(tl[i-1]) + int(tl[i+1])) / 2 for i,w in enumerate(tl)]
+        # Generator expression
+        lq = (int(w) if w.isdigit() else (int(tl[i-1]) + int(tl[i+1])) / 2 for i,w in enumerate(tl))
         [f.write(f"{i},") if i ** 0.5 >= less_v else "" for i in lq]
         f.write("\n")

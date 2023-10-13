d3679b0d17e736f8035898015fd9af558ee08c50
Your Name <a.efimov@infinet.ru>
2023-10-13 08:53:27 +0500

    P3 with comments

--- practice_1/P3.py
+++ practice_1/P3.py
@@ -16,3 +16,6 @@ with open(filename+"out", "w") as f:
         lq = (int(w) if w.isdigit() else (int(tl[i-1]) + int(tl[i+1])) / 2 for i,w in enumerate(tl))
         [f.write(f"{i},") if i ** 0.5 >= less_v else "" for i in lq]
         f.write("\n")
+
+
+# https://docs.python.org/3/howto/functional.html#generator-expressions-and-list-comprehensions

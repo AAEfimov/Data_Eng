6151cdcfc641a1af61d424d79cf183bbcf6a3c2a
Efimov Alexey <efimov.alexey.a@gmail.com>
2023-10-26 18:53:07 +0500

    p4_update

--- practice_2/P4.py
+++ practice_2/P4.py
@@ -1,2 +1,22 @@
+import json
 
+datafile = ""
+
+with open(datafile, mode="r") as f:
+    data = json.load(f)
+
+def price_update(product, price_info):
+    method = price_info["method"]
+    if method == "add":
+        product["price"] +=  price_info["param"]
+    elif method == "sub":
+        product["price"] -= price_info["param"]
+    elif method == "percent+":
+        product["price"] *= (1 + price_info["param"])
+    elif method == "percent-":
+        product["price"] *= (1 - price_info["param"])
+
+
+for prod, price_info in data.items():
+    price_update(prod, price_info)
 

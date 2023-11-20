52a423684926a12ab92ff123bdff98b60e9b648c
Your Name <a.efimov@infinet.ru>
2023-11-20 11:54:41 +0500

    P1 Done

--- practice_4/P1.py
+++ practice_4/P1.py
@@ -8,16 +8,64 @@ from aux_func import *
 
 
 varian = 40
-filename = "tests/1/filename"
+filename = "tests/1/task_1_var_40_item.text"
+dbname = "tests/1/ex1.db"
 
+json_out = "tests/1/{}.json"
 
-def main():
-    pass
+def parse_data(file):
 
+    with open(file, mode="r") as f:
+        lines = f.readlines()
 
-if __name__ == "__main__":     
-    conn, cursor = create_connection(filename)
-    main()
+    elist = []
+    item = {}
+    for l in lines:
+        if '=====' in l:
+            item['pages'] = int(item['pages'])
+            item['published_year'] = int(item['published_year'])
+            item['rating'] = float(item['rating'])
+            item['views'] = int(item['views'])
+ 
+            elist.append(item)
+            item = {}
+        else:
+            els = l.split("::")
+            item[els[0].strip()] = els[1].strip()
+
+    return elist
+
+if __name__ == "__main__":
+    table_name = 'books'   
+    conn, cursor = create_connection(dbname)
+    try:
+        cursor.execute('CREATE TABLE {} \
+            	(id INTEGER PRIMARY KEY, \
+                   title TEXT, \
+                   author TEXT, \
+                   genre TEXT, \
+                   pages INTEGER, \
+                   published_year INTEGER, \
+                   isbn TEXT, \
+                   rating REAL, \
+                   views INTEGER)'.format(table_name))
+    except sqlite3.OperationalError:
+        print("Table exist")
+
+    # data = parse_data(filename)
+    # fill_database(conn, cursor, data, table_name)
+
+    ex1 = json_out.format("ex1")
+
+    get_data_limit(conn, cursor, table_name, varian + 10,  'published_year', ex1)
+    print_sum_min_max(conn, cursor, table_name, 'rating')
+    print_freqes(conn, cursor, table_name, 'author')
+
+    ex4 = json_out.format("ex4")
+
+    get_data_filtered(conn, cursor, table_name, 35000, 'views', varian + 10, 'pages', ex4)
+
+    conn.close()
 
 
 
--- practice_4/aux_func.py
+++ practice_4/aux_func.py
@@ -1,10 +1,59 @@
 
 import sqlite3
 from sqlite3 import Error
+import json
 
 def create_connection(db_file):
     connection = sqlite3.connect(db_file)
     cursor = connection.cursor()
     return connection, cursor
 
+def fill_database(conn, cursor, data, table_name):
+    columns = ', '.join(data[0].keys())
+    placeholders = ', '.join('?' * len(data[0]))
+    sql_request = 'INSERT INTO {} ({}) VALUES ({})'.format(table_name, columns, placeholders)
 
+    for d in data:
+        values = [x for x in d.values()]
+        cursor.execute(sql_request, values)
+
+    conn.commit()
+
+def add_to_dict(m, fdict):
+    m = m.lower()
+    if m in fdict:
+        fdict[m] += 1
+    else:
+        fdict.setdefault(m, 1)
+
+def dict_filter_gt(val, fil):
+    return lambda x: x[fil] > val
+
+def dict_filter_lt(val, fil):
+    return lambda x: x[fil] < val
+
+def print_sum_min_max(conn, cursor, table_name, fild):
+    result = cursor.execute(f"SELECT MIN({fild}) as min_v, MAX({fild}) as max_v, SUM({fild}) as sum, COUNT({fild}) as cnt from {table_name}")
+    res = list(*result)
+    print("rating results")
+    print(f"MIN {res[0]}, MAX {res[1]}, SUM {round(res[2], 2)}, CNT {res[3]} AVG {round(res[2] / res[3], 2)}")
+
+def print_freqes(conn, cursor, table_name, fild):
+    
+    df = {}
+    res = cursor.execute(f"SELECT {fild} from {table_name}")
+    for f in res.fetchall():
+        add_to_dict(f[0], df)
+    print(df)
+
+def get_data_limit(conn, cursor, table_name, val, ord_fild, json_filename):
+    # DESC for UP to DOWN
+    result = cursor.execute("SELECT * FROM {} ORDER BY {} LIMIT {} ".format(table_name, ord_fild, val))
+    with open(json_filename, mode='w') as f_json:
+        json.dump([*result], f_json)
+
+
+def get_data_filtered(conn, cursor, table_name, filter_cnt, filter_fild, out_cnt, sort_fild, json_filename):
+    result = cursor.execute(f"SELECT * FROM {table_name} WHERE {filter_fild} > {filter_cnt} ORDER BY {sort_fild} DESC LIMIT {out_cnt}")
+    with open(json_filename, mode='w') as f_json:
+        json.dump([*result], f_json)

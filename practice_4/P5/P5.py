c84728d768fdd0821329f67d9578c111583e8bd1
Your Name <a.efimov@infinet.ru>
2023-11-22 10:27:14 +0500

    P5 Done

--- practice_4/P5/P5.py
+++ practice_4/P5/P5.py
@@ -15,11 +15,87 @@ game_databe = 'game.db'
 persons_namelist = 'pers_names.txt'
 person_out = 'persons.csv'
 
+json_out = "request_out/{}_out.json"
+
+def get_chars_with_trolley(conn, cursor, limit = 10):
+    result = cursor.execute("SELECT * FROM chars WHERE trolley == 1 ORDER BY def DESC LIMIT ?", [limit])
+    write_data_to_json([dict(r) for r in result.fetchall()], json_out.format("q1"))
+
+def count_char_by_weapon(conn, cursor):
+    result = cursor.execute("SELECT weapon_name, COUNT(*) AS count FROM chars GROUP BY weapon_name ORDER BY count DESC")
+    write_data_to_json([dict(r) for r in result.fetchall()], json_out.format("q2"))
+
+def get_axe_weapon_names_by_chars(conn, cursor):
+    result = cursor.execute("SELECT name, weapon_id, (SELECT name FROM Axe WHERE id = weapon_id ) AS Axe_name FROM chars WHERE weapon_name = 'Axe'")
+    write_data_to_json([dict(r) for r in result.fetchall()], json_out.format("q3"))
+
+
+def get_chars_with_max_atk_mace(conn, cursor):
+    # default Atk + Weapon Atk
+    # Не понимаю почему сложение с Алиасом не сработало
+    # wepon_atk + Atk AS total_atk
+
+    result = cursor.execute("SELECT name, weapon_name, weapon_id, Atk,  \
+                             (SELECT Atk FROM Mace WHERE id = weapon_id) AS wepon_atk, \
+                             Atk + (SELECT Atk FROM Mace WHERE id = weapon_id) as total_atk \
+                             FROM chars WHERE weapon_name = 'Mace' ORDER BY total_atk DESC")
+          
+    write_data_to_json([dict(r) for r in result.fetchall()], json_out.format("q4"))
+
+def get_chars_def(conn, cursor):
+
+    result = cursor.execute("SELECT name, Def, Head as head_id, Bracelet as brace_id, Armour as arm_id, Shoes as shoes_id , Robe as robe_id, Accessory as acc_id, \
+                            Def + (SELECT Def FROM Head WHERE id = Head) + \
+                            (SELECT Bracelet FROM Head WHERE id = Bracelet) + \
+                            (SELECT Armour FROM Head WHERE id = Armour) + \
+                            (SELECT Shoes FROM Head WHERE id = Shoes) + \
+                            (SELECT Robe FROM Head WHERE id = Robe) + \
+                            (SELECT Accessory FROM Head WHERE id = Accessory)\
+                            AS Total_def FROM chars ORDER BY Total_def DESC")
+
+    write_data_to_json([dict(r) for r in result.fetchall()], json_out.format("q5"))
+
+def get_chars_all_armour_names(conn, cursor, limit = 20):
+
+    result = cursor.execute("SELECT name, Head as head_id, Bracelet as brace_id, Armour as arm_id, Shoes as shoes_id , Robe as robe_id, Accessory as acc_id, \
+                            (SELECT name FROM Head WHERE id = Head) as nHead, \
+                            (SELECT name FROM Head WHERE id = Bracelet) as nBracelet, \
+                            (SELECT name FROM Head WHERE id = Armour) as nArmour, \
+                            (SELECT name FROM Head WHERE id = Shoes) as nShoes, \
+                            (SELECT name FROM Head WHERE id = Robe) as nRobe, \
+                            (SELECT name FROM Head WHERE id = Accessory) as nAccessory\
+                            FROM chars ORDER BY name LIMIT  ?", [limit])
+
+    write_data_to_json([dict(r) for r in result.fetchall()], json_out.format("q6"))
+
+
+def update_chars_armour(conn, cursor):
+
+    result = cursor.execute("UPDATE chars SET Armour = 50+RANDOM()%50")
+    #cursor.commit()
+    result = cursor.execute("SELECT * FROM chars")
+
+    write_data_to_json([dict(r) for r in result.fetchall()], json_out.format("q7"))
+
 if __name__ == '__main__':
 
     #generate_persons(persons_namelist, person_out)
     #create_and_fill_databases(game_databe, equipmen_zip, person_out)
 
     conn, cursor = create_connection(game_databe)
+
+    get_chars_with_trolley(conn, cursor)
+
+    count_char_by_weapon(conn, cursor)
+
+    get_axe_weapon_names_by_chars(conn, cursor)
+
+    get_chars_with_max_atk_mace(conn, cursor)
+
+    get_chars_def(conn, cursor)
+
+    get_chars_all_armour_names(conn, cursor)
+
+    update_chars_armour(conn, cursor)
 
     conn.close()
